05111bbdf6064d252b2d223afe91337e911f1a3c
abed-laptop <alavi.khorasani@gmail.com>
2020-04-03 17:47:40 +0430

    Add other currencies!

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const https = require('http');
+const request = require('request');
 var CronJob = require('cron').CronJob;
 const TelegramBot = require('node-telegram-bot-api');
 const token = "1117341186:AAEdTNCuXyPHUCLOaDlQcQgEwCyijHCqoIg"; //bitcoin
 // const token = "1029352844:AAEXUaoppERzW3ypp2OedZVZoIfXWY0tryo"; // testLocal
-const options = new URL('http://api.coindesk.com/v1/bpi/currentprice.json');
+
 const abedID = 629284243;
 const bot = new TelegramBot(token, {
     polling: {
@@ -31,21 +31,22 @@ process.on('exit', (code) => {
 });
 bot.sendMessage(abedID, new Date() + ": Program Started!").catch(e => {});
 /////////////////////////////////////////////////////////////////////////
-var price = 0;
+
 var job = new CronJob('* * * * *', function () {
-    const req = https.request(options, res => {
-        res.on('data', d => {
-            let result = JSON.parse(d);
-            bot.sendMessage(`@bitcoin_gheymat`, `\n${result.bpi.USD.rate} 💲USD\n\n@bitcoin_gheymat `);
-           
-        })
-    })
-
-    req.on('error', error => {
-        console.error(error)
-    })
-
-    req.end()
+    request('http://min-api.cryptocompare.com/data/pricemulti?fsyms=BTC,ETH,LTC,DOGE&tsyms=USD&api_key=40c6a434d7e306a35fa81943078ca30b652be0a09dce8fdc5f38399fc2169ef3', {
+        json: true
+    }, (err, res, body) => {
+        if (err) {
+            console.log(err);
+            bot.sendMessage(abedID,err);
+        }
+        let BTC = res.body.BTC.USD;
+        let ETH = res.body.ETH.USD;
+        let LTC = res.body.LTC.USD;
+        let DOGE = res.body.DOGE.USD;
+        // console.log(BTC, ETH, LTC, DOGE);
+        bot.sendMessage(`@bitcoin_gheymat`,`Bitcoin: ${BTC} 💲 USD\n\nEthereum: ${ETH} 💲 USD\n\nLitecoin: ${LTC} 💲 USD\n\nDogecoin: ${DOGE} 💲 USD\n\n👉@bitcoin_gheymat`);
+    });
 }, null, true, 'America/Los_Angeles');
 job.start();
 ////////////////////start/////////////////
@@ -71,9 +72,9 @@ async function mainMenu(id) {
             "parse_mode": "Markdown",
             "reply_markup": {
                 "keyboard": [
-                    ["دربارۀ بیت کوین", `روش نصب coinomi`,"احراز هویت"],
-                    ["خرید بیت‌کوین", `فروض بیت‌کوین`,"فروش کیف پول سخت‌افزاری"],
-                    
+                    ["دربارۀ بیت کوین", `روش نصب coinomi`, "احراز هویت"],
+                    ["خرید بیت‌کوین", `فروض بیت‌کوین`, "فروش کیف پول سخت‌افزاری"],
+
                 ],
                 "one_time_keyboard": true
             }
